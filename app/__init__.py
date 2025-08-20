a965c6379e605452f2a57880c6ee8faa4124b9af
Paris769 <andreacannillo@hotmail.com>
2025-08-20 08:47:59 -0700

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+"""Modulo di utilità per l'applicazione di riordino.
+
+Questo modulo rende disponibili le funzioni principali all'avvio dell'app Streamlit.
+"""
+
+__all__ = [
+    "parsing",
+    "io_excel",
+    "rules",
+    "reporting",
+]
